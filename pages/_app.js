8dd2366747ef60b64932322b27f3dc40cfbdddeb
setcoproject41-rgb <setcoproject41@gmail.com>
2025-11-19 13:42:02 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,6 @@
+// pages/_app.js
+import '@/styles/globals.css'; // Ganti jika Anda tidak menggunakan globals.css
 
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
